b755477fd4b4ecfa09045af3ad0246af1f3460c2
simonjung13 <148765577+simonjung13@users.noreply.github.com>
2023-11-16 17:27:34 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+
+def main():
+    print("Hi")
+    #snowapp_helper = Snowapp(resort_url_list= resort_url_list, powder_alert = 30, Test = False)
+    #snowapp_helper.fetchData_nightrun()
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
